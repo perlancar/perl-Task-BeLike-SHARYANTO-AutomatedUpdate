6c9924ce73293a2b8541a5198a963a671d70c139
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-10-02 11:35:10 +0700

    Initial commit

---
+++ lib/Task/BeLike/SHARYANTO/AutomatedUpdate.pm
@@ -0,0 +1,29 @@
+package Task::BeLike::SHARYANTO::AutomatedUpdate;
+
+# VERSION
+
+=head1 DESCRIPTION
+
+List of all SHARYANTO's modules that can be updated automatically by the author
+with a script.
+
+This includes all/most modules which use L<http://gudangdata.org> as source.
+
+=pkgroup Included modules
+
+=pkg Business::BankCard::Indonesia
+
+=pkg Business::BankList::Indonesia
+
+=pkg Locale::ID::Locality
+
+=pkg Locale::ID::Province
+
+=pkg Task::BeLike::SHARYANTO::Authored
+
+=pkg Task::BeLike::SHARYANTO::Favorited
+
+=cut
+
+1;
+# ABSTRACT: SHARYANTO's modules that can be updated automatically by the author with a script
